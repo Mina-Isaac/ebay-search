7677d6cd542134371b44ed7034b22d3e90161e79
Mina-Isaac <mina.isaac.2000@gmail.com>
2021-10-01 14:27:31 +0200

    AppID updated and CORS proxy enabled

--- src/services.ts
+++ src/services.ts
@@ -1,9 +1,11 @@
 import { ResponseAPI } from "./store/types";
 import { generateSearchURL, QueryParams } from "./utils";
 
+const CORSProxy = "https://thingproxy.freeboard.io/fetch/"
+
 const service = {
   searchByKeywords(queryParams: QueryParams): Promise<ResponseAPI> {
-    return fetch(generateSearchURL(queryParams)).then((res) => res.json());
+    return fetch(CORSProxy + generateSearchURL(queryParams)).then((res) => res.json());
   },
 };
 
